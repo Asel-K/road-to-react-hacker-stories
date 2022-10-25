cb5006d2e2c0808806d4eb8069bb70675a071453
Asel-K <asel.karagazieva@gmail.com>
2022-10-25 12:15:52 -0500

    Embeded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+// const title = 'React'
+
+function getTitle(title){
+  return title
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World {getTitle('World')}</h1>
+
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type="text" />
     </div>
   )
 }
